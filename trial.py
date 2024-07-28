4d1e341fd76250280407c246058d804dd061f261
Nida <tmarn2004@gmail.com>
2024-07-27 23:13:55 -0700

    "Added Person class with __init__ and __str__ methods, and created two instances p1 and p2 with names and ages."

---
+++ trial.py
@@ -0,0 +1,12 @@
+class Person:
+    def __init__(mysillyobjext,name,age):
+        mysillyobjext.name=name
+        mysillyobjext.age=age
+    def __str__(self) -> str:
+        pass
+        print(f"Hello my name is {abc.name} and my age is {abc.age}")
+
+p1=Person("Shyna",21)
+p2=Person("Tiffany",20)
+p1.myfunc()
+p2.myfunc()
